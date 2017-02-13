571daf8025424338c5b868ecd199940294befb89
Bruno <Bruno@172.30.26.148>
2017-02-13 17:03:00 +0000

    mapa inicial

--- Trabalho1-Nuno/src/Main.java
+++ Trabalho1-Nuno/src/Main.java
@@ -2,9 +2,16 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		// TODO Auto-generated method stub
-		//teste blalabalnalkkogfssfh  asndaksdSDASD
+		System.out.println("| X | X | X | X | X | X | X | X | X | X |");
+		System.out.println("| X | H |   |   | I |   | X |   | G | X |");
+		System.out.println("| X | X | X |   | X | X | X |   |   | X |");
+		System.out.println("| X |   | I |   | I |   | X |   |   | X |");
+		System.out.println("| X | X | X |   | X | X | X |   |   | X |");
+		System.out.println("| I |   |   |   |   |   |   |   |   | X |");
+		System.out.println("| I |   |   |   |   |   |   |   |   | X |");
+		System.out.println("| X | X | X |   | X | X | X | X |   | X |");
+		System.out.println("| X |   | I |   | I |   | X | k |   | X |");
+		System.out.println("| X | X | X | X | X | X | X | X | X | X |");	
 	}
 
 }
