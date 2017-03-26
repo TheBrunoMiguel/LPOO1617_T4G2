53e0e96cdf4c1985ebac3823316b745dc166a7dc
Bruno Miguel <up201504781@gcloud.fe.up.pt>
2017-03-26 18:28:18 +0100

    Working on Iteration 3

--- Lpoo Project 1/src/test/TestKeepGameLogic.java
+++ Lpoo Project 1/src/test/TestKeepGameLogic.java
@@ -1,26 +1,55 @@
 package test;
 
-//import static org.junit.Assert.*;
+import gameLogic.Ogre;
+import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 import org.junit.Test;
 
-//import gameLogic.Game;
+import gameLogic.Game;
+import gameLogic.Hero;
+
 
 public class TestKeepGameLogic 
 {
-	char [][] map = {{'X', 'X', 'X', 'X', 'X'},	
-					{'X', 'H', ' ', 'G', 'X'}, 
-					{'I', ' ', ' ', ' ', 'X'},
-					{'I', 'k', ' ', ' ', 'X'},
-					{'X', 'X', 'X', 'X', 'X'}};
-
-	String theguardmovement[] = {"down","down","left","up","left","up","right","right"};
-
-	
-	
-	//TESTES PARA O OGRE:
-	@Test
-	public void testHeroIsDefeatedByOgre()
+	public class TestDungeonGameLogic 
 	{
+		char [][] map = {{'X', 'X', 'X', 'X', 'X', 'X', 'X'}, 
+				{'X', ' ', ' ', ' ', ' ' ,' ', 'X'}, 
+				{'X', ' ', ' ', ' ', ' ', ' ', 'X'}, 
+				{'X', ' ', ' ', ' ', ' ', ' ', 'X'}, 
+				{'X', ' ', ' ', ' ', ' ', ' ', 'X'}, 
+				{'X', 'O', 'H', ' ', ' ', ' ', 'X'},
+				{'X', 'X', 'X', 'X', 'X', 'X', 'X'}};
 		
+		String theOgreMovement[]={"up","right","left","down"};
+		
+		String testOgreMovement[]={"up","up","up","up"};
+		
+		char [][] map2 = {{'X', 'X', 'X', 'X', 'X', 'X', 'X'}, 
+				{'I', ' ', ' ', ' ', ' ' ,'k', 'X'}, 
+				{'X', ' ', ' ', ' ', ' ', ' ', 'X'}, 
+				{'X', ' ', ' ', 'H', ' ', ' ', 'X'}, 
+				{'X', ' ', ' ', ' ', ' ', ' ', 'X'}, 
+				{'X', ' ', ' ', ' ', ' ', ' ', 'X'},
+				{'X', 'X', 'X', 'X', 'X', 'X', 'X'}};
+		
+		
+		
+		
+		@Test
+		public void testDoesOgreDeafeatHero()
+		{
+			Hero theHero = new Hero(2, 5);
+			Ogre theOgre = new Ogre(5, 5, theOgreMovement);
+			
+			Game game = new Game(map, theOgreMovement, 1, theHero);
+			
+			theHero.moveUp();
+			theOgre.update();
+			
+			game.checkSurround(theHero.getx(), theHero.gety());
+			assertFalse(game.getRunning());
+		}
 	}
 }
